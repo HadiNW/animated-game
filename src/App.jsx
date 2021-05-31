bc1a3a1884a3f00de767bb99938fa84fa900cde4
Hadi Nur <hadi.wahid@smartfren.com>
2021-05-31 21:38:03 +0700

    add nest, egg and electric spritesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react'
+
 import background from './assets/background.jpg'
 import board from './assets/board.png'
 import windowImg from './assets/window.png'
 import idleChar from './assets/char_idle.png'
 import egg from './assets/blue_egg.svg'
 import nest from './assets/nest.svg'
+import lamp from './assets/lamp.svg'
+import leafLeft from './assets/leaf_left.svg'
+import leafRight from './assets/leaf_right.svg'
+import electric from './assets/electricity_big.png'
 
 import Spritesheet from 'react-responsive-spritesheet'
 
 import './styles/index.scss'
 
 function App() {
+
+	const [buttonClicked, setButtonClicked] = useState(false)
+
 	return (
 		<div className='main-page'>
 			<div className='main-bg'>
@@ -38,9 +47,42 @@ function App() {
 					left: '-15%',
 				}}
 			/>
+			{buttonClicked && <Spritesheet
+				className='idle-char'
+				image={electric}
+				widthFrame={350}
+				heightFrame={350}
+				fps={10}
+				loop={true}
+				steps={8}
+				autoplay={true}
+				style={{
+					position: 'absolute',
+					zIndex: 10,
+				}}
+			/>}
 			<div className='egg-section'>
-				<img className='blue-egg' src={egg} alt='bluegg' />
-				<img src={nest} alt='nest' />
+				<div>
+					<img className='blue-egg' src={egg} alt='bluegg' />
+				</div>
+				<div>
+					<img className='nest' src={nest} alt='nest' />
+				</div>
+				<div>
+					<img className='lamp' src={lamp} alt='lamp' />
+				</div>
+			</div>
+
+			<div className="bottom-section">
+				<div className="left-leaf">
+					<img src={leafLeft} alt="left grass" />
+				</div>
+				<div className="main-button">
+					<button className='primary-button'>100</button>
+				</div>
+				<div className="right-leaf">
+					<img src={leafRight} alt="right grass" />
+				</div>
 			</div>
 		</div>
 	)
